7c1075c5af2b7ee4f277c09e0840380df4fcd866
Giulia Schiazzano <giulia.schiazzano@gmail.com>
2023-10-12 23:06:10 +0200

    se non va mi sparo

--- src/store/index.js
+++ src/store/index.js
@@ -16,24 +16,25 @@ export default createStore({
         category: "DJ",
         at: "Ex Macello",
         image: require("@/assets/media/open_rhythm.jpeg"),
-        description: "Open Rhythm by CULT x Belli di Notte torna per l'ultima volta sul dancefloor dell'Ex Macello il 12/10/2023. 7 ore di musica tra dj set ed esibizioni live. ",
+        description:
+          "Open Rhythm by CULT x Belli di Notte torna per l'ultima volta sul dancefloor dell'Ex Macello il 12/10/2023. 7 ore di musica tra dj set ed esibizioni live. ",
         age: "16+",
-        presentedBy:"CULT",
+        presentedBy: "CULT",
         lineup: [
           {
             artistName: "La Sape",
-            image: require("@/assets/media/artists_img/thayder.jpeg")
+            image: require("@/assets/media/artists_img/thayder.jpeg"),
           },
           {
             artistName: "Giorgio Thayder",
-            image: require("@/assets/media/artists_img/thayder.jpeg")
+            image: require("@/assets/media/artists_img/thayder.jpeg"),
           },
           {
             artistName: "Aznamir",
-            setTime: "21:45 - 23:15"
-          }
+            setTime: "21:45 - 23:15",
+          },
           // Aggiungi altri artisti e orari secondo necessità
-        ]
+        ],
       },
       {
         id: 2,
@@ -48,9 +49,10 @@ export default createStore({
         category: "party",
         at: "ARCA",
         image: require("@/assets/media/jungla.jpeg"),
-        description: " Il 12 ottobre ci ritroviamo nella sala Bistrot di Arca Milano per festeggiare la nascita di Jungla Records. Un’occasione speciale per iniziare una nuova storia e ascoltare le prime produzioni della nuova label meneghina.",
+        description:
+          " Il 12 ottobre ci ritroviamo nella sala Bistrot di Arca Milano per festeggiare la nascita di Jungla Records. Un’occasione speciale per iniziare una nuova storia e ascoltare le prime produzioni della nuova label meneghina.",
         age: "18+",
-        presentedBy:"6AM snc",
+        presentedBy: "6AM snc",
       },
       {
         id: 3,
@@ -77,9 +79,9 @@ export default createStore({
         }),
         category: "dj",
         image: require("@/assets/media/purple_disco.jpeg"),
-        description:"Siamo orgogliosi di presentare in esclusiva nazionale lo show di uno degli artisiti più famosi del pianeta, Purple Disco Machine che ha raggiunto il successo nel 2020 grazie al singolo Hypnotized prodotto in collaborazione con il gruppo Sophie and the Giants e divenuto uno dei tormentoni dell'estate.",
-        age: "Questo è un evento 18+"
-
+        description:
+          "Siamo orgogliosi di presentare in esclusiva nazionale lo show di uno degli artisiti più famosi del pianeta, Purple Disco Machine che ha raggiunto il successo nel 2020 grazie al singolo Hypnotized prodotto in collaborazione con il gruppo Sophie and the Giants e divenuto uno dei tormentoni dell'estate.",
+        age: "Questo è un evento 18+",
       },
       {
         id: 5,
@@ -94,9 +96,11 @@ export default createStore({
         }),
         category: "social",
         image: require("@/assets/media/sneakerness.avif"),
-        description: "questo ticket e' valido per il primo giorno di evento: 14 ottobre dalle 10 alle 21",
-        further_details: "PRIORITY TICKET E' VALIDO PER IL PRIMO GIORNO DI EVENTO: 14 OTTOBRE 2023 CON ACCESSO ANTICIPATO ALLE ORE 9 (INVECE CHE ALLE 10) FINO ALLE ORE 21  Europe's leading Sneaker & Lifestyle Convention is coming to Milano! 14-15 OCT 2023 H.10/21 EAST END STUDIOS- MILAN Via Mecenate 88/A BAMBINI GRATIS FINO AI 12 ANNI CON DOCUMENTO DI IDENTITA' ACCESSO AGEVOLATO PER I DISABILI LIBERO ACCESSO AI CANI",
-        age: "Tutte le età"
+        description:
+          "questo ticket e' valido per il primo giorno di evento: 14 ottobre dalle 10 alle 21",
+        further_details:
+          "PRIORITY TICKET E' VALIDO PER IL PRIMO GIORNO DI EVENTO: 14 OTTOBRE 2023 CON ACCESSO ANTICIPATO ALLE ORE 9 (INVECE CHE ALLE 10) FINO ALLE ORE 21  Europe's leading Sneaker & Lifestyle Convention is coming to Milano! 14-15 OCT 2023 H.10/21 EAST END STUDIOS- MILAN Via Mecenate 88/A BAMBINI GRATIS FINO AI 12 ANNI CON DOCUMENTO DI IDENTITA' ACCESSO AGEVOLATO PER I DISABILI LIBERO ACCESSO AI CANI",
+        age: "Tutte le età",
       },
       {
         id: 6,
@@ -111,9 +115,11 @@ export default createStore({
         }),
         category: "Artist Signing",
         image: require("@/assets/media/piuttosto_che.jpeg"),
-        description: '24/10 - Prima Data di Milano del Tour di Presentazione di "Niente di che" il nuovo romanzo di Pierluca Mariti (PIUTTOSTO_CHE). \n 18.30 Apertura \n 19.30 Presentazione + Firmacopie',
-        further_details: "Prima e dopo lo Spettacolo presso il Bar / Bistrot di Arca Milano sarà possibile mangiare e bere qualcosa nell'attesa oppure rilassarsi per quattro chiacchere dopo la presentazione.",
-        age: "Tutte le età"
+        description:
+          '24/10 - Prima Data di Milano del Tour di Presentazione di "Niente di che" il nuovo romanzo di Pierluca Mariti (PIUTTOSTO_CHE). \n 18.30 Apertura \n 19.30 Presentazione + Firmacopie',
+        further_details:
+          "Prima e dopo lo Spettacolo presso il Bar / Bistrot di Arca Milano sarà possibile mangiare e bere qualcosa nell'attesa oppure rilassarsi per quattro chiacchere dopo la presentazione.",
+        age: "Tutte le età",
       },
       {
         id: 7,
@@ -127,10 +133,12 @@ export default createStore({
         }),
         category: "film",
         image: require("@/assets/media/sneakerness2.avif"),
-        description:" questo ticket e' valido per il secondo giorno di evento: 15 ottobre dalle 10 alle 21",
-        further_details: "Europe's leading Sneaker & Lifestyle Convention is coming to Milano! 14-15 OCT 2023 H.10/21 EAST END STUDIOS- MILAN Via Mecenate 88/A \n BAMBINI GRATIS FINO AI 12 ANNI CON DOCUMENTO DI IDENTITA' ACCESSO AGEVOLATO PER I DISABILI LIBERO ACCESSO AI CANI",
+        description:
+          " questo ticket e' valido per il secondo giorno di evento: 15 ottobre dalle 10 alle 21",
+        further_details:
+          "Europe's leading Sneaker & Lifestyle Convention is coming to Milano! 14-15 OCT 2023 H.10/21 EAST END STUDIOS- MILAN Via Mecenate 88/A \n BAMBINI GRATIS FINO AI 12 ANNI CON DOCUMENTO DI IDENTITA' ACCESSO AGEVOLATO PER I DISABILI LIBERO ACCESSO AI CANI",
         age: "Tutte le età",
-        presentedBy: "east market Milano"
+        presentedBy: "east market Milano",
       },
       {
         id: 8,
@@ -202,10 +210,12 @@ export default createStore({
         }),
         category: "party",
         image: require("@/assets/media/la_cler.jpeg"),
-        description:" Quartetto base composto da batteria, basso, chitarra e tastiere che aprirà lo spettacolo, poi i musicisti dello Studio La Sabbia lasceranno il passo a chiunque voglia suonare uno dei loro strumenti creando una situazione armoniosa e inclusiva.",
-        further_details: "Vuoi suonare uno strumento diverso? Portalo e raggiungilo sul palco. Vuoi cantare? Porta te stesso e raggiungili sul palco.",
+        description:
+          " Quartetto base composto da batteria, basso, chitarra e tastiere che aprirà lo spettacolo, poi i musicisti dello Studio La Sabbia lasceranno il passo a chiunque voglia suonare uno dei loro strumenti creando una situazione armoniosa e inclusiva.",
+        further_details:
+          "Vuoi suonare uno strumento diverso? Portalo e raggiungilo sul palco. Vuoi cantare? Porta te stesso e raggiungili sul palco.",
         age: "18+",
-        presentedBy: "Clèr"
+        presentedBy: "Clèr",
       },
 
       {
@@ -331,6 +341,20 @@ export default createStore({
 
       // Aggiungi altri eventi qui
     ],
+    // Carrello
+    contatore: 0,
   },
   // Mutations, actions e getters qui, se necessario
+  mutations: {
+    AGGIUNGI: function (state) {
+      if (state.contatore < 3) {
+        state.contatore += 1;
+      }
+    },
+    TOGLI: function (state) {
+      if (state.contatore > 0) {
+        state.contatore -= 1;
+      }
+    },
+  },
 });
